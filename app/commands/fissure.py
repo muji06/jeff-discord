8c637c39b438bbd0d68cb621977db3700bee22d0
afilopati19 <afilopati19@epoka.edu.al>
2022-09-25 18:09:10 +0200

    Fixing sorted fissures

--- app/commands/fissure.py
+++ app/commands/fissure.py
@@ -30,29 +30,32 @@ class fissure(commands.Cog):
             color=discord.Colour.random()
             )
 
+        fissure_list = []
+
         if f_type == '':
             for x in data:
                 if not x['isStorm'] and not x['isHard']:
-
-                    embed.add_field(name=f"{x['tier']} - {x['missionType']} - {x['enemy']}",
-                    value=f"{x['node']}{chr(10)}Ends in {x['eta']}",
-                    inline=False)
+                    fissure_list.append((x['tierNum'],f"{x['tier']} - {x['missionType']} - {x['enemy']}", f"{x['node']}{chr(10)}Ends in {x['eta']}"))
 
         elif f_type == 'Steel Path':
             for x in data:
                 if x['isHard']:
+                    fissure_list.append((x['tierNum'],f"{x['tier']} - {x['missionType']} - {x['enemy']}", f"{x['node']}{chr(10)}Ends in {x['eta']}"))
 
-                    embed.add_field(name=f"{x['tier']} - {x['missionType']} - {x['enemy']}",
-                    value=f"{x['node']}{chr(10)}Ends in {x['eta']}",
-                    inline=False)
 
         elif f_type == 'Railjack':
             for x in data:
                 if x['isStorm']:
-
-                    embed.add_field(name=f"{x['tier']} - {x['missionType']} - {x['enemy']}",
-                    value=f"{x['node']}{chr(10)}Ends in {x['eta']}",
-                    inline=False)    
+                    fissure_list.append((x['tierNum'],f"{x['tier']} - {x['missionType']} - {x['enemy']}", f"{x['node']}{chr(10)}Ends in {x['eta']}"))
+    
+        fissures_sorted = sorted(fissure_list, key=lambda x: x[0])
+        
+        for fissure in fissure_list:
+            embed.add_field(
+                name=fissure[1],
+                value=fissure[2],
+                inline=False
+            )
 
         embed.set_footer(text=f"Valid fissure types are: rj (Railjack), sp (Steel Path), <empty> (Normal)"+"\n"+f"Latency: {round((time.time() - start)*1000)}ms")
         await ctx.send(embed=embed)
