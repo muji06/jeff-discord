db90d75caaae33e62e5b87d0f60ee885d8ae5f9f
Aldi Filopati <afilopati19@epoka.edu.al>
2023-05-22 23:45:40 +0200

    pain

--- app/jefferson.py
+++ app/jefferson.py
@@ -14,7 +14,7 @@ bot = commands.Bot(command_prefix='-', intents=intents)
 async def load():
     for file in os.listdir('./commands'):
         if file.endswith('.py'):
-            await bot.load_extension(f'comms.{file[:-3]}') 
+            await bot.load_extension(f'commands.{file[:-3]}') 
 
 @bot.tree.command(name="sync")
 async def func(interaction: discord.Interaction):
