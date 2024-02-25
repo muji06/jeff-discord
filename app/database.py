85c0d3f82ab511dcb346304d8408950cd8639014
muji06 <k.filopati@gmail.com>
2024-02-25 20:02:45 +0100

    fixing models

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from os import path
 import logging
 from sqlalchemy import create_engine
-from sqlalchemy import String, ForeignKey, Integer
+from sqlalchemy import String, ForeignKey, Integer, JSON
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import mapped_column, relationship
 from sqlalchemy.orm import sessionmaker, scoped_session
@@ -19,22 +19,37 @@ Session = scoped_session(session_factory)
 class Base(DeclarativeBase):
     pass
 
-
+# separate list for each user
 class Watchlists(Base):
     __tablename__ = "watchlists"
 
     user_id = mapped_column(String(30), primary_key=True)
-    server_id = mapped_column(String(20), primary_key=True)
-    watch_list = mapped_column(String(30), nullable=True)
-    total_videos = mapped_column(Integer, default=0)
-    total_storage = mapped_column(Integer,  default=0)
+    watch_list = mapped_column(JSON, default=lambda: [])
+    # example watch list
+    # [
+    #     {
+    #         "from": "darvo",
+    #         "waiting_for": "Kohm",
+    #         "send_to": "dm",# or channel
+    #         "server": "1234567890",
+    #         "channel": "1234567890",
+    #     }
+    # ]
 
 class Listeners(Base):
     __tablename__ = "listeners"
 
-    listener_id = mapped_column(String(30), primary_key=True)
+    listener_name = mapped_column(String(30), primary_key=True)
     current_id = mapped_column(String(30), nullable=True)
-    __
+    send_to = mapped_column(JSON, default=lambda: [])
+    # [
+    #     {
+    #         "server": "1234567890",
+    #         "channel": "1234567890",
+    #         "ping": "<@1234567890>"
+    #     }
+    # ]
+
 def init_db():
     with engine.connect(): # just to start it
         Base.metadata.create_all(engine)
